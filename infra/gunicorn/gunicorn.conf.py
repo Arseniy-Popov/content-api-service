b8f0f0c7998e546dbf53139e479dcaf8ce8ea5e7
Arseniy Popov <arseniypopov@gmail.com>
2022-07-04 21:21:26 +0300

    Add Gunicorn plus Uvicorn auto reload note

--- infra/gunicorn/gunicorn.conf.py
+++ infra/gunicorn/gunicorn.conf.py
@@ -6,4 +6,7 @@ bind = "0.0.0.0:5000"
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = app_config.GUNICORN_WORKERS
 logconfig_dict = LOGGING
+
+# doesn't work yet
+# https://github.com/benoitc/gunicorn/issues/2339
 reload = app_config.GUNICORN_RELOAD
